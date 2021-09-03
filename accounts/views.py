44b9848208d9df67ddf693554a52934d258f60bc
Felipe Silva <felipesilva@192.168.1.5>
2021-09-03 03:39:00 -0300

    Loaded all data from database on templates.

--- accounts/views.py
+++ accounts/views.py
@@ -3,11 +3,31 @@ from .models import *
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 'customers': customers,
+               'total_customers': total_customers,
+               'total_orders': total_orders,
+               'pending': pending, 'delivered': delivered}
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    context = {'customer': customer,
+               'orders': orders, 'order_count': order_count}
+    return render(request, 'accounts/customer.html', context)
 
 
 def products(request):
