b0bf6a4fa27c56d4df60bb18236c998b341df46f
Felipe Silva <felipesilva@192.168.1.5>
2021-09-02 23:24:31 -0300

    Created all templates strutucture.

---
+++ accounts/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=25, null=True)
+    email = models.CharField(max_length=100, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pendente', 'Pendente'),
+        ('Saiu para entrega', 'Saiu para entrega'),
+        ('Entregue', 'Entregue'),
+    )
+    customer = models.ForeignKey(
+        Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(
+        max_length=100, null=True, choices=STATUS)
---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import *
+
+
+def home(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+def customer(request):
+    return render(request, 'accounts/customer.html')
+
+
+def products(request):
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
