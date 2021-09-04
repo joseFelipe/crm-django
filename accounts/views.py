65f0e0c2f6baf0b8a0e69027e4e1749246936b74
Felipe Silva <felipesilva@192.168.1.11>
2021-09-04 01:03:13 -0300

    Created multiline form for create orders.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import OrderForm
+from django.forms import inlineformset_factory
 
 
 def home(request):
@@ -36,16 +37,19 @@ def products(request):
     return render(request, 'accounts/products.html', {'products': products})
 
 
-def createOrder(request):
-    form = OrderForm()
-
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(
+        Customer, Order, fields=('product', 'status'), extra=10)
+    customer = Customer.objects.get(id=pk)
+    # form = OrderForm(initial={'customer': customer})
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('home')
 
-    context = {'form': form}
+    context = {'formset': formset}
     return render(request, 'accounts/order_form.html', context)
 
 
